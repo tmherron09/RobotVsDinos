83e281439e1b042c8e2766910ae46e9efa299415
TimH <tmherron09@gmail.com>
2020-07-13 12:16:05 -0500

    Add Class Fleet
    Add Class Fleet to project. Fleet stores List<Robot> robots for the 3 robots on battlefield. Fleet constructor will call function to initialize the list of robotos. Member variables for List<Robot> robots and List<Weapon> available weapons have been declared along with constructor, empty method for InitializeFleet() and InitializeNewRobot().

---
+++ RobotsVsDinosaurs/Fleet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotsVsDinosaurs
+{
+    class Fleet
+    {
+        List<Robot> robots;
+        List<Weapon> availableWeapons;
+
+        public Fleet()
+        {
+            robots = InitializeFleet();
+            availableWeapons = new List<Weapon>();
+        }
+
+        private List<Robot> InitializeFleet()
+        {
+            throw new NotImplementedException();
+            //List<Robot> robots;
+            //Robot robot = InitializeNewRobot();
+            //robots.Add(robot) 
+            // times 3
+            //return robots;
+        }
+        private Robot InitializeNewRobot()
+        {
+
+        }
+    }
+}
